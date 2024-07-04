7abfb79db882b1a964d39ac2aa93b175f15213f1
withcodes <gouravkumarjha003@gmail.com>
2024-07-04 19:16:39 +0530

    creating xpath and method for index page

--- MyStoreProject/src/main/java/pages/IndexPage.java
+++ MyStoreProject/src/main/java/pages/IndexPage.java
@@ -33,7 +33,51 @@ public class IndexPage extends BaseClass {
 	private WebElement signUpEmail;
 	
 	@FindBy(xpath = "//button[@data-qa='signup-button']")
-	 WebElement signUpBtn;
+	private WebElement signUpBtn;
+	
+	@FindBy(xpath = "//div[@class='login-form']/h2[@class='title text-center']/child::b")
+	private WebElement account_inforamtion_text;
+	
+	@FindBy(xpath = "//label[@for='id_gender1']")
+	private WebElement gender;
+	
+	@FindBy(xpath = "//input[@id='name']")
+	private WebElement verify_name;
+	
+	@FindBy(xpath = "//input[@id='email']")
+	private WebElement vrify_email;
+	
+	@FindBy(xpath = "//input[@id='password']")
+	private WebElement  fill_password;
+	
+	@FindBy(xpath = "//select[@id='days']")
+	private WebElement select_day;
+	
+	@FindBy(xpath = "//select[@id='months']")
+	private WebElement select_month;
+	
+	@FindBy(xpath = "//select[@id='years']")
+	private WebElement select_year;
+	
+	@FindBy(xpath = "//input[@id='newsletter']")
+	private WebElement checkbox_newsletter;
+	
+	@FindBy(xpath = "//input[@id='optin']")
+	private WebElement checkbox_spacialoffer;
+	
+	@FindBy(xpath = "//form[@action='/signup']/child::input[1]")
+	private WebElement first_name;
+	
+	@FindBy(xpath = "//input[@data-qa='last_name']")
+	private WebElement last_name;
+	
+	@FindBy(xpath = "//input[@data-qa='company']")
+	private WebElement company_name;
+	
+	@FindBy(xpath = "//input[@data-qa='address2']")
+	private WebElement address2;
+	
+	
 	
 	public void verifyurl()
 	{
